c5e6be29a1ce2afdf26b3a53e6fad0f2b764857d
udsingla <130015308+udsingla@users.noreply.github.com>
2024-05-14 18:39:32 -0700

    added step 5

--- __tests__/lab6.test.js
+++ __tests__/lab6.test.js
@@ -143,6 +143,13 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 5
     // At this point he item 'cart' in localStorage should be 
     // '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
+
+    const cart = await page.evaluate(() => { return localStorage.getItem('cart');} );
+
+    // Expected cart
+    const eCart = '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]';
+
+    expect(cart).toEqual(eCart);
     
   });
 
